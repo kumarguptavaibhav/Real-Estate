a009bece35837acc44b05cd2aec53a4bbddcb2f5
kumarguptavaibhav <vaibhav9161porwal@gmail.com>
2024-05-24 01:38:20 +0530

    ..

--- api/app.js
+++ api/app.js
@@ -3,7 +3,7 @@ import postRoute from "./routes/post.route.js"
 
 const app = express();
 
-app.use("/api/post", postRoute);
+app.use("/api/posts", postRoute);
 
 app.listen(8800, ()=>{
     console.log("server is running");
